9142b8e4e696fcfc601eba0a0ef3751fecea7bb1
Michael <Michael3495@gmail.com>
2015-01-28 20:26:36 +0100

    Initial Commit

---
+++ sharing/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  sharing
+//
+//  Created by Michael Gittens on 28/01/15.
+//  Copyright (c) 2015 Michael Gittens. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
